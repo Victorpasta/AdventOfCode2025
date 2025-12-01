46e0720ce07bbe0bee08921336ca448ae9385ca6
Alexander Slotte <alexander.slotte01@gmail.com>
2025-12-01 21:02:31 +0100

    Auto stash before merge of "main" and "origin/main"

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -8,6 +8,7 @@ namespace Tests
         [Fact]
         public void Puzzle1Test()
         {
+<<<<<<< Updated upstream
             var puzzle1 = new Puzzle1(50, "C:\\Users\\vicsch\\source\\personal\\AdventOfCode2025\\Day1\\Puzzle1\\Input.txt");
             var res = puzzle1.Run();
             
@@ -20,5 +21,11 @@ namespace Tests
             var res = puzzle2.Run();
 
         }
+=======
+            var res = Puzzle1.Run("C:\\Users\\Alexander\\Documents\\skola\\Gitrepo\\AdventOfCode2025\\Day1\\Puzzle1\\Input.txt");
+            
+        }
+     
+>>>>>>> Stashed changes
     }
 }
